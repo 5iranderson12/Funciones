fbee39841a48e0d02132e9b774dd80e0682a5c5b
AndersonSaulMaravillaCallejas <164236833+5iranderson12@users.noreply.github.com>
2024-10-21 14:53:06 -0600

    Create 2. Crea una función que reciba un array de números y devuelva el mayor de ellos.js

---
+++ 2. Crea una función que reciba un array de números y devuelva el mayor de ellos.js
@@ -0,0 +1,10 @@
+//Anderson Saul Maravilla Callejas
+
+function encontrarMayor(arr) {
+  return Math.max(...arr);
+}
+
+//Ejemplo
+let numeros = [3, 8, 12, 5, 22];
+let mayor = encontrarMayor(numeros);  // Devuelve 22
+console.log(mayor);
